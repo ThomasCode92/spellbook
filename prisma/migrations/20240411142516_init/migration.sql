d0f41ea8ad57d3a559feed63bd3c14feb094ac91
Thomas <thomascode92@hotmail.com>
2024-04-13 17:42:46 +0200

    Add Prisma (and SQLite) Support

---
+++ prisma/migrations/20240411142516_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "Spellbook" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Spell" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "image" TEXT,
+    "spellbookId" INTEGER NOT NULL,
+    CONSTRAINT "Spell_spellbookId_fkey" FOREIGN KEY ("spellbookId") REFERENCES "Spellbook" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Spellbook_title_key" ON "Spellbook"("title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Spell_title_key" ON "Spell"("title");
